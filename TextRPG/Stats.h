a28c2d45981e8d418d9c7873858c0014e0ccc274
Antonio La Rocca <antoniolarocca95@gmail.com>
2020-03-17 22:31:11 -0400

    * learned that you can't compile template classes into cpp files

--- TextRPG/Stats.h
+++ TextRPG/Stats.h
@@ -1,6 +1,55 @@
 #pragma once
 #include "BaseIncludes.h"
 
+template <typename E>
+class Stat
+{
+	static_assert(std::is_enum<E>::value, "Type E must be an enum");
+private:
+	E mStatType;
+	uint16 mBaseValue;
+	std::vector<int8> mModifiers;
+
+public:
+	Stat(E statType, uint16 startingValue)
+	{
+		mStatType = statType;
+		mBaseValue = startingValue;
+		mModifiers = std::vector<int8>();
+	};
+
+	int16 GetTotalModifier()
+	{
+		int16 total = 0;
+		for (auto attributeModifier : Stat::mModifiers)
+		{
+			total += attributeModifier;
+		}
+	};
+
+	const uint16& GetBaseValue()
+	{
+		return mBaseValue;
+	};
+
+	uint16 GetValue()
+	{
+		int16 modifier = GetTotalModifier();
+		if (modifier > mBaseValue)
+		{
+			return 0;
+		}
+
+		return mBaseValue - modifier;
+	};
+
+	uint16 AddModifier(int8 modifier)
+	{
+		mModifiers.push_back(modifier);
+		return GetValue();
+	};
+};
+
 enum class AttributeType
 {
 	Strength,
@@ -12,33 +61,6 @@ enum class AttributeType
 	LENGTH
 };
 
-enum class SkillType
-{
-	Test,
-	LENGTH
-};
-
-template <typename E>
-class Stat
-{
-	static_assert(std::is_enum<E>::value, "Type E must be an enum");
-private:
-	E mStatType;
-	uint16 mBaseValue;
-	std::vector<int8> mModifiers;
-
-public:
-	Stat(E statType, uint16 startingValue);
-
-	int16 GetTotalModifier();
-
-	const uint16& GetBaseValue();
-
-	uint16 GetValue();
-
-	uint16 AddModifier(int8 modifier);
-};
-
 class Attribute : public Stat<AttributeType>
 {
 public:
@@ -46,6 +68,12 @@ public:
 	static std::string AttributeString(AttributeType type);
 
 	Attribute(AttributeType attributeType) : Stat(attributeType, 10) {};
+};
+
+enum class SkillType
+{
+	Test,
+	LENGTH
 };
 
 class Skill : public Stat<SkillType>
